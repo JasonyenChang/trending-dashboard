0cb38e9868a106ed65b38e55d8fc56031097d1aa
JasonyenChang <jasonyen2008@gmail.com>
2025-10-21 15:43:08 +0800

    install tailwindcss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-8">
-    <h1 class="text-2xl font-bold mb-6 text-blue-800">熱門商品排行榜</h1>
+    <h1 class="text-2xl font-bold mb-6 text-orange-800">熱門商品排行榜</h1>
     <p>這是你的第一個 Vue 頁面 🎉</p>
   </div>
 </template>
